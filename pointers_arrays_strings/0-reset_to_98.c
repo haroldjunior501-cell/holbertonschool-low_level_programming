959dbff793555c521742c143210af7c55a2a1d20
haroldjunior501-cell <haroldjunior501@gmail.com>
2026-03-06 19:33:34 +0100

    pointers_arrays_strings: fix Betty style in 0-reset_to_98.c

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,10 @@
 #include "main.h"
 
+/**
+ * reset_to_98 - updates the value pointed to by n to 98
+ * @n: pointer to the int to update
+ */
 void reset_to_98(int *n)
 {
-    *n = 98;
+	*n = 98;
 }
