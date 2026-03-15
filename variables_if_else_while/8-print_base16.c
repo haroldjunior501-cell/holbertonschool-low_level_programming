58b2729fc811b5ea6fafa719f96b2eb5c76cec2a
haroldjunior501-cell <haroldjunior501@gmail.com>
2026-03-15 04:00:28 +0100

    variables_if_else_while: add 8-print_base16.c

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - prints all numbers of base 16 in lowercase
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char c;
+
+	c = '0';
+	while (c <= '9')
+	{
+		putchar(c);
+		c++;
+	}
+	c = 'a';
+	while (c <= 'f')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
