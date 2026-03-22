594f938ab43235d47cd6d5c0d179a827806e433e
haroldjunior501-cell <haroldjunior501@gmail.com>
2026-03-23 00:36:33 +0100

    structures_typedef: add 1-init_dog.c

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ * init_dog - initializes a variable of type struct dog
+ * @d: pointer to the struct dog to initialize
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ *
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
